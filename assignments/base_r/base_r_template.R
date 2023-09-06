5aa691699aa20e33f58ac0b66fc6ecb0a8903dea
Nicky Bell <nicky.bell@gmail.com>
2023-09-05 22:42:39 -0400

    Adds base R assignment files

---
+++ assignments/base_r/base_r_template.R
@@ -0,0 +1,68 @@
+#----------------------------------------------
+# SMPA 2152
+# Assignment #1
+# Name: YOUR NAME HERE
+# Worked with: NAMES HERE (if applicable)
+#----------------------------------------------
+
+# Please type your code into the sections outlined below. When a question asks you to interpret the results of your code, use # to write your answer as a comment.
+
+#----------------------------------------------
+# Question 1
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 2
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 3
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 4
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 5
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 6
+
+
+
+
+
+
+
+
