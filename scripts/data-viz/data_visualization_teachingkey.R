1bd3ee2ed745ee9d74868a04e20b32c797b1752b
Nicky Bell <nicky.bell@gmail.com>
2023-09-20 14:40:53 -0500

    Incorporates nbmisc package; code through multivariate graphs

--- scripts/data-viz/data_visualization_teachingkey.R
+++ scripts/data-viz/data_visualization_teachingkey.R
@@ -7,6 +7,8 @@
 library(tidyverse)
 library(nycflights13)
 library(palmerpenguins)
+# The package {nbmisc} is not publicly available, but you can download the development version if necessary using the command: remotes::install_github("nickybell/nbmisc")
+library(nbmisc)
 
 # We're going to jump right into making graphs using ggplot2
 
@@ -45,44 +47,30 @@ ggplot(data = flights) +
   flights$month_categories <- as.factor(flights$month)
   head(flights$month_categories)
   
-# The second thing to think about is how many variables you are graphing: univariate (one variable) or multivariate (2+ variables)?
-  
-ggplot() +
-  geom_hline(yintercept = 0) +
-  geom_vline(xintercept = -.5) +
-  geom_vline(xintercept = .5) +
-  labs(y = "Number of Variables",
-       x = "Type of Variables") +
-  scale_x_continuous(limits = c(-1.5, 1.5), labels = c("Discrete", "Continuous", "Combination"), breaks = c(-1, 0, 1), expand = c(0,0)) +
-  scale_y_continuous(limits = c(-1, 1), labels = c("Multivariate", "Univariate"), breaks = c(-.5, .5), expand = c(0,0)) +
-  annotate("text", x = -1, y = .5, label = "Bar graph") +
-  annotate("text", x = 0, y = .5, label = "Histogram") +
-  annotate("text", x = -1, y = -.5, label = "Box and whiskers plot\ntest") +
-  geom_rect(aes(xmin = .5, xmax = 1.5, ymin = 0, ymax = 1), fill = "gray") +
-  theme_minimal() +
-  theme(axis.text.y = element_text(angle = 90, hjust = .5),
-        axis.ticks = element_blank(),
-        panel.grid = element_blank())
-  
-  
+# The second thing to think about is how many variables you are graphing: univariate (one variable), bivariate (two variables), or multivariate (3+ variables)?
+
+# The combination of variable types and number of variables will determine which graph you should create.
+types_of_graphs() # this comes from the {nbmisc} package
+
 # Today we will be using the "penguins" data from the {palmerpenguins} package that you are also using for your homework.
   
 # Univariate Graphs -------------------------------------------------------
 
-# Univariate graph, discrete variable: bar graph
+# Discrete variable: bar graph
+# When working with univariate graphs, we are often just counting values
 
 ggplot(data = penguins) +
 	geom_bar(mapping = aes(x = species))
   
-  # If you are plotting a single statistic rather than a count:
+  # You can also pass in the count directly (e.g., what if you have a vector of means?)
   table(penguins$species)
   peng2 <- data.frame(species = c("Adelie", "Chinstrap", "Gentoo"),
-                      count = c(152, 68, 124))
+                      mean_body_mass_g = c(3701, 3733, 5076))
   
   ggplot(data = peng2) +
-    geom_bar(mapping = aes(x = species, y = count), stat = "identity")
+    geom_bar(mapping = aes(x = species, y = mean_body_mass_g), stat = "identity")
 
-# Univariate graph, continuous variable: histogram
+# Continuous variable: histogram
 
 ggplot(data = penguins) +
 	geom_histogram(mapping = aes(x = flipper_length_mm), binwidth = 10)
@@ -94,54 +82,93 @@ ggplot(data = penguins) +
 ggplot(data = penguins) +
 	geom_bar(aes(x = year))
 
-ggplot(data = penguins) +
-	geom_bar(aes(x = as.factor(year))) # Are years a numeric or categorical variable? Let's consider some examples.
+# Now remove the data from 2008 from the data frame. What change do you need to make to your graph code now? Hint: use as.factor()
+penguins_lim <- penguins[penguins$year %in% c(2007, 2009),]
 
-ggplot(data = penguins[penguins$year %in% c(2007, 2009),]) +
-	geom_bar(aes(x = year))
+ggplot(data = penguins_lim) +
+  geom_bar(aes(x = year))
 
-ggplot(data = penguins[penguins$year %in% c(2007, 2009),]) +
-	geom_bar(aes(x = as.factor(year)))
+ggplot(data = penguins_lim) +
+  geom_bar(aes(x = as.factor(year)))
 
-# Create the same graph, but using a custom data frame (3 rows, 3 columns) with a column called "count" that is the number of penguins studied in each year.
-table(penguins$year)
-peng2 <- data.frame(year = c(2007:2009),
-                    count = c(110, 114, 120))
-ggplot(data = peng2) +
-  geom_bar(aes(x = year, y = count), stat = "identity")
-
-# Multivariate Graphs -----------------------------------------------------
+# Bivariate Graphs -----------------------------------------------------
 
 # 2 discrete variables
 
 # What species of penguins live on each island?
+# It is useful to think of this like two nested counts: the number of penguins on each island, and then of those penguins, the number that come from each species
+penguins |>
+  group_by(island, species) |>
+  summarize(n_species = n()) |>
+  mutate(n_island = sum(n_species))
+
+# Bar graph
 
 ggplot(data = penguins) +
 	geom_bar(aes(x = island, fill = species), position = "dodge") +
 	scale_fill_discrete(type = c("aquamarine", "seagreen", "salmon"))
 
+colors()
+
+# 2D heat map (more complicated)
+
+ggplot(data = penguins) +
+  geom_bin_2d(aes(x = island, y = species)) +
+  theme_bw() +
+  theme(panel.grid = element_blank())
+
 # 1 discrete variable, 1 continuous variable
 
-# How does bill length (in centimeters) differ by penguins' sex?
+# How does bill length differ by penguins' sex?
+
+# Box and whisker plot
 
 ggplot(data = penguins[!is.na(penguins$sex),]) +
-	geom_boxplot(aes(x = sex, y = bill_length_mm/10)) +
+	geom_boxplot(aes(x = sex, y = bill_length_mm)) +
 	scale_y_continuous(limits = c(0, NA)) +
 	labs(x = "Sex",
-			 y = "Bill Length (cm)",
-			 title = "Penguin Bill Length (cm) by Sex",
+			 y = "Bill Length (mm)",
+			 title = "Penguin Bill Length by Sex",
 			 caption = "Source: palmerpenguins package")
+
+# Violin plot
+
+ggplot(data = penguins[!is.na(penguins$sex),]) +
+  geom_violin(aes(x = sex, y = bill_length_mm), fill = "steelblue") +
+  scale_y_continuous(limits = c(0, NA)) +
+  labs(x = "Sex",
+       y = "Bill Length (mm)",
+       title = "Penguin Bill Length by Sex",
+       caption = "Source: palmerpenguins package")
 
 # 2 continuous variables
 
-# Are flipper length and body mass (kg) related?
+# Scatterplot
+
+# Are flipper length and body mass related?
 
 ggplot(data = penguins) +
-	geom_point(aes(x = flipper_length_mm, y = body_mass_g/1000)) +
-	scale_y_continuous(limits = c(0, NA)) +
-	geom_smooth(aes(x = flipper_length_mm, y = body_mass_g/1000), method = "lm")
+	geom_point(aes(x = flipper_length_mm, y = body_mass_g)) +
+	geom_smooth(aes(x = flipper_length_mm, y = body_mass_g), method = "lm", se = F, linetype = "dashed", size = 2)
 
-# Let's add in a third variable
+# Why is it acceptable to leave the axis values where they are rather than set them to 0? Because we are interested in the relationship between these variables rather than the values themselves.
+
+# Line graph
+# This usually occurs when the y-axis is ordered, like time. For example, let's create a data frame that shows the mean bill length for each year of the study. You'll learn how to understand this code in the next module of the course.
+peng3 <- 
+  penguins |> 
+  group_by(year) |> 
+  summarize(mean_bill_length_mm = mean(bill_length_mm, na.rm = T))
+
+ggplot(data = peng3, mapping = aes(x = year, y = mean_bill_length_mm)) +
+  geom_line() +
+  geom_point() +
+  scale_y_continuous(limits = c(0, NA))
+
+
+# Multivariate Graphs -----------------------------------------------------
+
+# Let's make the same line graph, but this time, we are going to separate the data by the sex.
 
 ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g/1000, color = species)) +
 	geom_point() +
